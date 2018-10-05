06dc2b29671e7a1f81f40288e2086d416640e167
Jen-Tang Cheng <lunagrissom@gmail.com>
2018-10-05 20:22:17 +0800

    Add files via upload

---
+++ ex13.py
@@ -0,0 +1,15 @@
+# exp13.py python3
+
+from sys import argv
+#script, first, second, third = argv
+
+print("The script is called:", end=' ')
+script = input()
+print("Your first variable is:", end=' ')
+first = input()
+print("Your second variable is:", end=' ')
+second = input()
+print("Your third variable is:", end=' ')
+third = input()
+
+print(f"The script is called {script},Your first variable is {first}, second is {second}, and third is {third}. ")
